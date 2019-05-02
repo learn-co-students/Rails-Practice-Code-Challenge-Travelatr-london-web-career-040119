09d3587fe9c519c2bdf0dbf5807f0ad26ae3f7b7
Ivan Szebenszki <szib77@gmail.com>
2019-05-02 15:23:29 +0100

    Add likes and featured posts to user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :bloggers, only: %i[new create show]
+  resources :posts, only: %i[new create edit update show]
 end

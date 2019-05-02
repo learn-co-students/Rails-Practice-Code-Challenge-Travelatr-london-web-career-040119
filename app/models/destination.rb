a8b679752cb4072d4ecd86775a943495fd87f2fd
Ivan Szebenszki <szib77@gmail.com>
2019-05-02 14:46:57 +0100

    Add most_recent_posts to destination

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,8 @@
 class Destination < ApplicationRecord
   has_many :posts
   has_many :bloggers, through: :posts
+
+  def most_recent_posts
+    posts.order(creation_time: :desc).limit(5)
+  end
 end

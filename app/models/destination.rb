9274d62b85ebfab61ecb8e2d04289e97e6a680c5
endyranaudo <endyranaudo@gmail.com>
2019-05-02 15:50:00 +0100

    destination show page done

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -3,16 +3,20 @@ class Destination < ApplicationRecord
   has_many :bloggers, through: :posts
 
   def most_recent_posts
-    posts.order(creation_time: :desc).limit(5)
+    Post.where("destination_id = ?", self.id).order(creation_time: :desc).limit(5)
   end
 
   def unique_bloggers
-    self.users.uniq
+    self.bloggers.uniq
   end
 
   def average_bloggers_age
     ages = unique_bloggers.map {|b| b.age }
     ages.sum / ages.count
   end
-  
+
+  def featured_posts
+    Post.where("destination_id = ?", self.id).order(likes: :desc).limit(5)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :bloggers, only: %i[new create show]
   resources :posts, only: %i[new create edit update show]
+  resources :destinations, only: %i[show]
 end

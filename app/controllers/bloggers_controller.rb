26a1a118b4b917f5bc60c79bfcec18b80751d106
Ivan Szebenszki <szib77@gmail.com>
2019-05-02 15:55:15 +0100

    Remove clutter form post

--- app/controllers/bloggers_controller.rb
+++ app/controllers/bloggers_controller.rb
@@ -1,17 +1,11 @@
 class BloggersController < ApplicationController
-  before_action :find_blogger, except: %i[index new create]
-
-  def index
-    @bloggers = Blogger.all
-  end
+  before_action :find_blogger, only: :show
 
   def show; end
 
   def new
     @blogger = Blogger.new
   end
-
-  def edit; end
 
   def create
     @blogger = Blogger.new(blogger_params)
@@ -21,26 +15,6 @@ class BloggersController < ApplicationController
     else
       flash[:error] = 'Something went wrong'
       render 'new'
-    end
-  end
-
-  def update
-    if @blogger.update_attributes(blogger_params)
-      flash[:success] = 'Blogger was successfully updated'
-      redirect_to @blogger
-    else
-      flash[:error] = 'Something went wrong'
-      render 'edit'
-    end
-  end
-
-  def destroy
-    if @blogger.destroy
-      flash[:success] = 'Blogger was successfully deleted'
-      redirect_to @bloggers_path
-    else
-      flash[:error] = 'Something went wrong'
-      redirect_to @bloggers_path
     end
   end
 

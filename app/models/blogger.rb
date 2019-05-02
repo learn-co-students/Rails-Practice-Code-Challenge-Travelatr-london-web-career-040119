771acebd53504471f3694d44db88f4c4131374f5
endyranaudo <endyranaudo@gmail.com>
2019-05-02 13:55:35 +0100

    relations done

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,7 @@
 class Blogger < ApplicationRecord
+  has_many :posts
+  has_many :destinations, through: :posts
 
+  # Bloggers should have unique names and ages above 0, and their bio should be over 30 characters long
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
+  belongs_to :blogger
+  belongs_to :destination
 
+  # must have title
+
+  # content longer than 100 characters
+  
 end
